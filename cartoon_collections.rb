ccfbe571f4ba51c547cfedf98c0bbbd35e5b6c30
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-28 02:02:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.include?(cheese) ? find.cheese : nil
+  cheese_types.include?(cheese) ? cheese_types.find(cheese) : nil
 end
